ec6cc9868cf757e33d8a4bac9fcffaf123632749
Wang Ying <wy867504146@outlook.com>
2021-02-04 22:55:10 +0800

    提交 use-sdk-demo 代码

---
+++ use-test-sdk/src/main/java/UseTestSdk.java
@@ -0,0 +1,8 @@
+import pers.wangying.testsdk.TestSdk;
+
+public class UseTestSdk {
+    public static void main(String[] args) {
+        TestSdk testSdk = new TestSdk();
+        testSdk.greeting();
+    }
+}
